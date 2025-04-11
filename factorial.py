fd10fcfc3d9d03f40b84ecf4a138eb47abcd8a3e
Fernando Araujo <jfac2420@gmail.com>
2025-04-11 18:59:06 -0500

    hice factorial y tabla

---
+++ factorial.py
@@ -0,0 +1,7 @@
+factorial = 1 
+
+for a in range(1, 6):
+    factorial *= a
+    print(f"El factorial de {a} es: {factorial}")
+    
+print(factorial)
---
+++ tabla-multiplicar.py
@@ -0,0 +1,3 @@
+num = int(input("Ingrese un numero a multiplicar: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
