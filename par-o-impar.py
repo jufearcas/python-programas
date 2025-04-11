9adf773b01214d595004735f447512afa7fdd26b
Fernando Araujo <jfac2420@gmail.com>
2025-04-11 18:36:28 -0500

    agregue suma y validar impar

---
+++ par-o-impar.py
@@ -0,0 +1,7 @@
+
+num = int(input("Ingrese un numero: "))
+
+if num % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
---
+++ suma.py
@@ -0,0 +1,6 @@
+
+
+numA = int(input("Ingrese el primer numero"))
+numB = int(input("Ingrese el segundo numero"))
+suma = numA + numB
+print("La suma de los dos numeros es: ", suma)
